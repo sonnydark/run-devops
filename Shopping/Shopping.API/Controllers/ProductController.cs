82e347f4dc09a5d667a25f99364ebb2c1413d2a5
Erik Petráš <erik.petras@m2ms.sk>
2023-03-30 09:05:37 +0200

    trigger api and client pipeline

--- Shopping/Shopping.API/Controllers/ProductController.cs
+++ Shopping/Shopping.API/Controllers/ProductController.cs
@@ -5,7 +5,6 @@ using Shopping.API.Data;
 using Shopping.API.Models;
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace Shopping.API.Controllers
--- Shopping/Shopping.Client/Controllers/HomeController.cs
+++ Shopping/Shopping.Client/Controllers/HomeController.cs
@@ -5,7 +5,6 @@ using Shopping.Client.Models;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
 
